6dead38fe37ccaf40542712be324f77b76604053
natefrog808 <wetakatini@gmail.com>
2025-02-04 16:37:14 -1000

    Create constants.js

---
+++ src/components/TrainingEnvironment/constants.js
@@ -0,0 +1,18 @@
+// Challenge Types
+export const CHALLENGE_TYPES = {
+  PATTERN: 'pattern',
+  OPTIMIZATION: 'optimization'
+};
+
+// Pattern Types
+export const PATTERN_TYPES = {
+  ARITHMETIC: 'arithmetic',
+  GEOMETRIC: 'geometric',
+  QUADRATIC: 'quadratic',
+  EXPONENTIAL: 'exponential'
+};
+
+// Learning Tracks
+export const LEARNING_TRACKS = {
+  PATTERN_RECOGNITION: 'PATTERN_RECOGNITION'
+};
