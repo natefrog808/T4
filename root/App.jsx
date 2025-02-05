5a1caa2ec54db63a00abb8a873a6212818507004
natefrog808 <wetakatini@gmail.com>
2025-02-04 16:11:38 -1000

    Create App.jsx

---
+++ root/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import TrainingEnvironment from './components/TrainingEnvironment';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <TrainingEnvironment />
+    </div>
+  );
+}
+
+export default App;
